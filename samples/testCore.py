51c88f00bba3f0439a27d42dfd1170efc22a7128
Michael Welch <michaelgwelch@gmail.com>
2012-03-30 00:38:39 -0500

    Remove testCore script and with that the last of the python scripts

--- samples/testCore.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-lineNumber = 0
-def assertEquals(x):
-	global lineNumber
-	lineNumber = lineNumber + 1
-	input = raw_input()
-	if (x == input): return
-	print "Line %d: expected:'%s' != '%s'" % (lineNumber,x,input)
-
