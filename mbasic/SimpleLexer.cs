cca9dd9a5b71b8f2b28338f3e30759beb4751420
Michael Welch <michaelgwelch@gmail.com>
2012-05-22 20:06:14 -0500

    Add SimpleLexer empty class

--- mbasic/SimpleLexer.cs
+++ mbasic/SimpleLexer.cs
@@ -9,3 +9,14 @@ namespace mbasic
     }
 }
 
+// Tokens
+
+// Line Numbers are 15 bit signed integers
+// Quoted Strings - these are the normal String Literals
+// Strings - this can be non-quoted strings in DATA statements or variables or numbers in a DATA statement - the runtime may have to figure it out.
+// Floating Point - this is number literal
+// Keywords
+// Operators
+
+
+
